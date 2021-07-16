fdbe939eb67da19a6428e5e2cb5f207e8125035d
Daniel Souza <danisztls@gmail.com>
2021-07-15 23:21:31 -0300

    Rewrote script to universal use

--- i18n.js
+++ i18n.js
@@ -1,42 +1,88 @@
-/* @title   i18n Automatic
- * @version 0.1
- * @author  danisztls
- * @license ISC */
- 
-let userLang
+/* @name    i18nRedirector
+ * @desc    Redirect user on 1st visit to internationalized alternative of thepage based on navigator language. 
+// Language is stored in local storage and further visits results in a noop.
+ * @version 1.0
+ * @author  Daniel <danisztls@gmail.com>
+ * @license MIT */
 
-function langDetect () {
-  // get lang from localStorage
-  userLang = localStorage.getItem('userLang')
+function i18nRedirect() {
+  // Options
+  const defaultLang = "en" // leave blank to redirect to root
+  const siteLangs = ["en", "de"] // array of languages supported, order by preference
+  const baseURL = "" // if site has a base URL (e.g. for 'https://mysite.com/doc/' use 'doc/')
 
-  // if lang is not set
-  if (!userLang) {
-    // get locale from browser
-    let browserLocale = navigator.language || navigator.userLanguage
-    // TODO: refactor to work with multiple languages, currently it's hardcoded
-    // derive lang from locale
-    if (browserLocale.match(/es/)) {
-      userLang = 'es'
+  // Main
+  // noop if already redirected
+  if (!localStorage.getItem("isRedirected")) {
+    // get lang from browser preferences
+    let userLang = navigator.language || navigator.userLanguage
+
+    // try regionalized first (e.g. en-us)
+    let redirectLang = siteLangs.find((e) => e.match(userLang))
+
+    // if lang not found try internationalized (e.g. en)
+    if (!redirectLang) {
+      userLang = userLang.replace(/^([a-z]{2}).*$/, "$1")
+      redirectLang = siteLangs.find((e) => e.match(userLang))
+    }
+
+    // if still not found use default
+    if (!redirectLang) {
+      redirectLang = defaultLang
+    }
+
+    //FIXME: Redirect to root if empty defaultLang
+
+    // set redirect boolean on local storage
+    localStorage.setItem("isRedirected", true)
+
+    // get page path
+    let pagePath = document.location.pathname
+
+    // trim baseURL
+    if (baseURL != "") {
+      pagePath = pagePath.replace(baseURL, "")
+    }
+
+    // analyze path
+    let isPathRegional = false // e.g. /en-US/introduction
+    if (pagePath.match(/^\/[a-z]{2}-\w{2}\/.*$/)) {
+      isPathRegional = true
+    }
+
+    let isPathInternational = false // e.g. /en/introduction
+    if (pagePath.match(/^\/[a-z]{2}\/.*$/)) {
+      isPathRegional = true
+    }
+
+    let isPathRoot = true // e.g. /introduction
+    if (isPathRegional || isPathInternational) {
+      isPathRoot = false
+    }
+
+    let replaceRE
+    let replaceFormat = `/${redirectLang}/$1`
+
+    if (isPathRoot) {
+      replaceRE = /^\/(.*)$/
+    } else if (isPathRegional) {
+      replaceRE = /^\/[a-z]{2}\/(.*)$/
     } else {
-      userLang = 'en'
+      // isPathInternational
+      replaceRE = /^\/[a-z]{2}-\w{2}\/(.*)$/
+    }
+
+    // compute redirect path
+    let redirectPath = pagePath.replace(replaceRE, replaceFormat)
+
+    if (baseURL != "") {
+      // insert baseURL on path
+      redirectPath = redirectPath.replace(/^\/(.*)$/, `/${baseURL}$1`)
+    }
+
+    // redirect if different from current path
+    if (pagePath != redirectPath) {
+      window.location.replace(redirectPath)
     }
   }
 }
-
-function langRedirect (redirectLang) {
-  // set lang on localStorage
-  localStorage.setItem('userLang', redirectLang)
-  
-  // compute path
-  let pageURL = document.location.href
-  let langRE = "$1/" + redirectLang + "/$2"
-  let redirectURL = pageURL.replace(/(.*)\/[a-z]{2}\/(.*)/, langRE)
-
-  // redirect if not the same
-  if (pageURL != redirectURL) {
-    window.location.replace(redirectURL)
-  }
-}
-
-langDetect()
-langRedirect(userLang)
